7489672017b87d983a7e0b310ff0827d762b90e0
Arjon29 <arjon.bela@gmail.com>
2021-07-18 13:20:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// var logo = document.querySelector('.container2');
+// var menu = document.querySelector('.menu');
+
+// logo.addEventListener('click', function() {
+//     menu.classList.toggle('showmenu')
+
+// });
+
+console.log("sfqsfs");
